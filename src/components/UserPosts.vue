bb4acf97807ae47fa7795364f133226586c19564
NormanMises <normanmises@gmail.com>
2023-07-21 23:48:17 +0800

    动态-数据存储

--- src/components/UserPosts.vue
+++ src/components/UserPosts.vue
@@ -1,5 +1,9 @@
 <template>
+    <div class="card">
+        <div class="card-body">
 
+        </div>
+    </div>
 </template>
 
 <script>
--- src/views/UserProfileView.vue
+++ src/views/UserProfileView.vue
@@ -4,7 +4,9 @@
             <div class="col-3">
                 <UserInfo/>
             </div>
-            <div class="col-9">约炮记录</div>
+            <div class="col-9">
+                <UserPosts/>
+            </div>
         </div>
     </ContentBase>
 </template>
@@ -12,12 +14,29 @@
 <script>
 import ContentBase from "@/components/ContentBase.vue";
 import UserInfo from "@/components/UserInfo.vue";
+import UserPosts from "@/components/UserPosts.vue";
+import {reactive} from "vue";
 
 export default {
     name: "UserProfileView",
     components: {
         ContentBase,
-        UserInfo
+        UserInfo,
+        UserPosts
+    },
+    setup() {
+        const user = reactive({
+            id: 1,
+            username: "yhf",
+            lastName: "y",
+            firstName: "hf",
+            followerCount: 0,
+            is_followed: false,
+        })
+
+        return {
+            user,
+        }
     }
 }
 </script>
