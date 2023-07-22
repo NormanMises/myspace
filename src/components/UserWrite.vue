12ccd156bc354fd4a34dd17898c6fcc647a24ffc
NormanMises <normanmises@gmail.com>
2023-07-22 23:18:06 +0800

    动态-发帖 绑定submit函数

--- src/components/UserWrite.vue
+++ src/components/UserWrite.vue
@@ -2,15 +2,29 @@
     <div class="card edit-field">
         <div class="card-body">
             <label for="edit-post" class="form-label">编辑帖子</label>
-            <textarea class="form-control" id="edit-post" rows="3"></textarea>
-            <button type="button" class="btn btn-primary btn-sm">发帖</button>
+            <textarea v-model="content" class="form-control" id="edit-post" rows="3"></textarea>
+            <button @click="submit" type="button" class="btn btn-primary btn-sm">发帖</button>
         </div>
     </div>
 </template>
 
 <script>
+import {ref} from "vue";
+
 export default {
-    name: "UserWrite"
+    name: "UserWrite",
+    setup() {
+        let content = ref('');
+
+        const submit = () => {
+            content.value = '';
+        }
+        return {
+            content,
+            submit,
+        }
+
+    }
 }
 </script>
 
