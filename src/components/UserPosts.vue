7b7acc84590c7d985d1b65fe703afcb157ff7eb9
NormanMises <normanmises@gmail.com>
2023-07-28 00:05:37 +0800

    用户列表-内容中用变量用 {{ }}, 标签中用变量用 v-bind 简写为 : 。

--- src/components/UserPosts.vue
+++ src/components/UserPosts.vue
@@ -4,6 +4,7 @@
             <div v-for="post in posts.posts" :key="post.id">
                 <div class="card single-post">
                     <div class="card-body">
+<!--                        内容中用变量用 {{ }} -->
                         {{ post.content }}
                     </div>
                 </div>
--- src/views/UserListView.vue
+++ src/views/UserListView.vue
@@ -1,6 +1,19 @@
 <template>
     <ContentBase>
-        炮友列表
+        <div class="card" v-for="user in users" :key="user.id">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-1">
+                        <!--                        标签中用变量用 v-bind 简写为 : -->
+                        <img class="img-fluid" :src="user.photo" alt="">
+                    </div>
+                    <div class="col-11">
+                        <div class="username">{{ user.username }}</div>
+                        <div class="folllower-count">{{ user.followerCount }}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </ContentBase>
 </template>
 
@@ -34,5 +47,28 @@ export default {
 
 
 <style scoped>
+img {
+    border-radius: 50%;
+}
 
+.card {
+    margin-bottom: 20px;
+    cursor: pointer;
+    transition: 500ms;
+}
+
+.card:hover{
+    box-shadow: 2px 2px 10px lightgray;
+}
+
+.username {
+    font-weight: bold;
+    height: 50%;
+}
+
+.folllower-count {
+    font-size: 12px;
+    color: gray;
+    height: 50%;
+}
 </style>
