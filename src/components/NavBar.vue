8e9fbe1608977f5aa5d2b9c05e8c4ad697fe8c9e
NormanMises <normanmises@gmail.com>
2023-08-06 23:15:11 +0800

    登录-登录后右上角显示头像和登出

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,38 +1,60 @@
 <template>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container">
-    <router-link class="navbar-brand" :to="{name:'Home'}">我的后宫</router-link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <router-link class="nav-link" :to="{name:'Home'}">首页</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" :to="{name:'UserList'}">炮友列表</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" :to="{name:'UserProfile', params:{userId: 2}}">炮友动态</router-link>
-        </li>
-      </ul>
-     <ul class="navbar-nav">
-        <li class="nav-item">
-          <router-link class="nav-link" :to="{name:'Login'}">登录</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" :to="{name:'Register'}">注册</router-link>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container">
+            <router-link class="navbar-brand" :to="{name:'Home'}">我的后宫</router-link>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+                    aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <router-link class="nav-link" :to="{name:'Home'}">首页</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" :to="{name:'UserList'}">炮友列表</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" :to="{name:'UserProfile', params:{userId: 2}}">炮友动态
+                        </router-link>
+                    </li>
+                </ul>
+                <ul class="navbar-nav" v-if="!$store.state.user.is_login">
+                    <li class="nav-item">
+                        <router-link class="nav-link" :to="{name:'Login'}">登录</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" :to="{name:'Register'}">注册</router-link>
+                    </li>
+                </ul>
+                <ul class="navbar-nav" v-else>
+                    <li class="nav-item">
+                        <router-link class="nav-link" :to="{name:'UserProfile', params:{userId: $store.state.user.id}}">
+                            {{ $store.state.user.username }}
+                        </router-link>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" style="cursor: pointer" @click="logout">登出</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 </template>
 
 <script>
+import {useStore} from "vuex";
+
 export default {
     name: "NavBar",
+    setup() {
+        const store = useStore();
+        const logout = () => {
+            store.commit('logout');
+        };
+
+        return {logout,}
+    }
 }
 </script>
 
